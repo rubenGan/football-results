06e612fc381fd7f66714b5a0e20071a1a564ee11
Rubén <ruben.vanyo@gmail.com>
2019-10-22 17:22:10 +0200

    list of results league

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -11,19 +11,79 @@ import { HttpClient } from '@angular/common/http';
 export class ResultsComponent implements OnInit {
   matchDay: matchDay;
   private apiKey: string= utils.apiKey;
+  private leagueCode:string = utils.leagueCode;
+  private year:string = utils.year;
+  public leaguesList:any =[    
+    {
+    "Text":"Premier League",
+    "Value":"PL"
+  },
+  {
+    "Text":"Brazilian Division One",
+    "Value":"BSA"
+  },
+  {
+    "Text":"Champions league",
+    "Value":"CL"
+  },
+  {
+    "Text":"Europa league",
+    "Value":"EC"
+  },
+  {
+    "Text":"France League 1",
+    "Value":"FL1"
+  },
+  {
+    "Text":"Eredivisie",
+    "Value":"DED"
+  },
+  {
+    "Text":"Bundesliga",
+    "Value":"BL1"
+  },
+  {
+    "Text":"La Liga Santander",
+    "Value":"PD"
+  },
+  {
+    "Text":"Championship",
+    "Value":"ELC"
+  },
+  {
+    "Text":" Italy Serie A",
+    "Value":"SA"
+  },
+  {
+    "Text":"World Cup",
+    "Value":"PL"
+  },
+  {
+    "Text":"Liga NOS",
+    "Value":"PPL"
+  }
+];
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get('http://api.football-data.org/v2/competitions/PL/matches/?matchday=2',{
+    this.http.get('http://api.football-data.org/v2/competitions/'+ this.leagueCode +'/matches/?matchday=2',{
       headers: { 'X-Auth-Token': this.apiKey },
       responseType: 'json'
     })
     .subscribe((data: matchDay) => {
-      debugger;
       this.matchDay = data;
-      console.log(this.matchDay);
   });
+}
+
+changeLeague(codeLeague) {
+  this.http.get('http://api.football-data.org/v2/competitions/'+ codeLeague +'/matches/?matchday=2',{
+    headers: { 'X-Auth-Token': this.apiKey },
+    responseType: 'json'
+  })
+  .subscribe((data: matchDay) => {
+    this.matchDay = data;
+});
 }
 
   }
--- src/environments/globals.ts
+++ src/environments/globals.ts
@@ -5,3 +5,5 @@
 
 export const version: string="0.0.1";
 export const apiKey:string ="d2fcf78996184af497d50a069b1c348b";   //from: https://www.football-data.org/documentation/samples
+export const leagueCode:string = "PL";
+export const year:string = "2019";
